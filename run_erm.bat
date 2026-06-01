c886e1cedf5bcf07c13ff337085ceed30178ecf5
Alena Stavrova <33433876+Alena-Stavrova@users.noreply.github.com>
2026-06-01 10:45:52 +0300

    Update run_erm.bat
    Fixed errors

--- run_erm.bat
+++ run_erm.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Starting Tier 2 tests...
-python LVH_script_runner.py
+echo Starting Ermenrich tests...
+python ERM_script_runner.py
 echo.
 echo Tests finished. Press any key to close.
 pause
